e02e42dbeb35c905d648aa192e599f70b42aa480
kuro-kuroite <kurokuro33.black000@gmail.com>
2018-01-23 23:21:59 +0900

    create build environment and init project file, #5

---
+++ bin/init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+# FIXME: どのディレクトリからでもcd APP_ROOT/bin/ に移動できるようにしたい 
+# 現在は ROOT_APP からの実行(`./bin/coffeec.sh`)だけでも対処出来るように cd ./bin としている  
+cd ./bin >& /dev/null 
+cd ..
+
+
+npm init -y
+
+npm install --save-dev coffeescript && coffee -v
+
+npm install --save-dev babel-cli
+npm install --save-dev babel-preset-es2015
+
+echo '{ "presets": ["es2015"] }' >> .babelrc
+
+npm install --save-dev ts2c
+
+npx
+npm install 
